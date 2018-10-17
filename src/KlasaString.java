6b021974a8d35fd9b56a4d1d0f510b39ceaeb14d
Ammar Berkovic <ammar.berkovic@gmail.com>
2018-10-17 16:47:31 +0200

    Prvi commit

---
+++ src/KlasaString.java
@@ -0,0 +1,32 @@
+
+public class KlasaString {
+String neki;
+	
+	
+	public int duzinaStringa() {
+		return 0;
+	}
+	
+	public void karakteriNaParnim() {
+		
+	}
+	
+	public void karakteriNaNeparnim() {
+		
+	}
+	
+	public int brojUppercase() {
+		return 0;
+	}
+	
+	public int brojLowercase() {
+		return 0;
+	}
+	
+	public void nisuSlova() {
+			
+		
+	}
+
+}
+
