db9ba09f21a7adff19f7225825e9a3aadbeb31e0
AmraSelimovic <44226479+AmraSelimovic@users.noreply.github.com>
2018-10-19 09:29:20 +0200

    Update KlasaString.java

--- src/KlasaString.java
+++ src/KlasaString.java
@@ -44,12 +44,19 @@ String neki;
 	}
 	
 	public int brojLowercase() {
-		
+		int brojac=0;
+		for(int i=0; i<neki.length(); i++) {
+			if(Character.isLowerCase(neki.charAt(i))) {
+				brojac++;
+			}
+		}
+		return brojac;
 	}
 	
 	public void nisuSlova() {
-		
-			
+		for(int i=0; i<neki.length(); i++) {
+			if(Character.isLetter(neki.charAt(i))==false && neki.charAt(i)!=' ') 
+				System.out.print(neki.charAt(i));
 		
 	}
 
